67021b58b87fe71b4e7b51b46acb3a3bbdf1190a
antonmartinus73 <antonmartinus72@gmail.com>
2020-01-13 10:50:06 +0700

    new

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-class MainApp():
+from core.baseapp import BaseApp
+
+class MainApp(BaseApp):
     def __init__(self):
         self.books = []
-
 
 if __name__ == "__main__":
     app = MainApp()
     app.run()
-
